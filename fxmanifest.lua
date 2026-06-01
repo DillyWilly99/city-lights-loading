bed45df0657fb3af92d6c9f7a7c1b6dc7b2a22a0
DillyWilly99 <codi.work2018@gmail.com>
2026-06-01 14:28:38 -0500

    Initial commit: Add QBCore loading screen with music support

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'City Lights RP'
+description 'City Lights RP Loading Screen'
+version '1.0.0'
+
+loadscreen 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js',
+    'html/music.mp3'
+}
